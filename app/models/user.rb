608125017838e795e800bb0832241f3ee20b8aaa
Bryon Winger <bryonwinger@gmail.com>
2017-02-25 14:55:16 -0600

    add scaffolding for Article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :circuits
+  has_many :articles
 
   # Favorites
   has_many :user_circuit_favorites
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225181428) do
+ActiveRecord::Schema.define(version: 20170225204624) do
+
+  create_table "articles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "circuits", force: :cascade do |t|
     t.string   "name"
